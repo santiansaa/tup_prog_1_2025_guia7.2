8b81ab819ad237d7afc2028ed5e6436139e99aca
santiago ansa <santiagoansa4@gmail.com>
2025-06-05 23:36:21 -0300

    entrega guia 7-2

---
+++ lista_ordenada/lista_ordenada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_ordenada
+{
+    class lista_ordenada
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
